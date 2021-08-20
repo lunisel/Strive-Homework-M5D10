a8f4472e0dc81d952b957c75665b1fd0ad71af67
Luna Sarmiento <selene942008@hotmail.com>
2021-08-20 16:29:26 +0200

    Added reviews

--- src/media/index.js
+++ src/media/index.js
@@ -2,6 +2,9 @@ import express from "express";
 import uniqid from "uniqid";
 import createHttpError from "http-errors";
 import { getMedia, writeMedia } from "../../lib/fs-tools.js";
+/* import { CloudinaryStorage } from "multer-storage-cloudinary";
+import { v2 as cloudinary } from "cloudinary";
+import multer from "multer"; */
 
 const mediaRouter = express.Router();
 
@@ -64,6 +67,74 @@ mediaRouter.delete("/:imdbID", async (req, resp, next) => {
   try {
     const media = await getMedia();
     const remainingMedia = media.filter((m) => m.imdbID !== req.params.imdbID);
+    await writeMedia(remainingMedia);
+    resp.status(204).send();
+  } catch (err) {
+    next(err);
+  }
+});
+
+/* const cloudStorage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "M5D10-NetflixAPI",
+  },
+}); */
+
+/* const cloudMulter = multer({ storage: cloudStorage });
+
+mediaRouter.post(
+  "/:imdbID/poster",
+  cloudMulter.single("poster"),
+  async (req, resp, next) => {
+    try {
+      const newMedia = { Poster: req.file.path };
+      resp.send("Uploaded!");
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+ */
+mediaRouter.post("/:imdbID/reviews", async (req, resp, next) => {
+  try {
+    const media = await getMedia();
+    const remainingMedia = media.filter((m) => m.imdbID !== req.params.imdbID);
+    const singleMedia = media.find((m) => m.imdbID === req.params.imdbID);
+    const reviews = singleMedia.Reviews;
+    const newMedia = {
+      ...singleMedia,
+      Reviews: [
+        ...reviews,
+        {
+          ...req.body,
+          _id: uniqid(),
+          elementId: req.params.imdbID,
+          createdAt: new Date(),
+        },
+      ],
+    };
+
+    remainingMedia.push(newMedia);
+    await writeMedia(remainingMedia);
+    resp.send(newMedia.Reviews);
+  } catch (err) {
+    next(err);
+  }
+});
+
+mediaRouter.delete("/:imdbID/reviews/:_id", async (req, resp, next) => {
+  try {
+    const media = await getMedia();
+    const remainingMedia = media.filter((m) => m.imdbID !== req.params.imdbID);
+    const singleMedia = media.find((m) => m.imdbID === req.params.imdbID);
+    const reviews = singleMedia.Reviews;
+    const remainingReviews = reviews.filter((r) => r._id !== req.params._id);
+    const newMedia = {
+      ...singleMedia,
+      Reviews: [...remainingReviews],
+    };
+    remainingMedia.push(newMedia);
     await writeMedia(remainingMedia);
     resp.status(204).send();
   } catch (err) {
